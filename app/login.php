18cae20810ed0e7449e37f7414fa098dd7d89e7b
Tim Visée <timvisee@gmail.com>
2015-08-31 00:03:52 +0200

    Created login page logic

--- app/login.php
+++ app/login.php
@@ -1,19 +1,18 @@
 <?php
 
+use app\mail\MailManager;
 use app\session\SessionManager;
-use app\team\Team;
-use app\team\TeamManager;
 use app\template\PageFooterBuilder;
 use app\template\PageHeaderBuilder;
+use app\user\User;
+use app\user\UserManager;
+use app\util\AccountUtils;
 
 // Include the page top
 require_once('top.php');
 
-$loginUser = null;
-$loginPass = null;
-
 // Check whether the user is trying to login, if not show the login form instead
-if(!isset($_POST['login_user']) || !isset($_POST['login_pass'])) {
+if(!isset($_POST['login_user']) || !isset($_POST['login_password'])) {
     ?>
     <div data-role="page" id="page-login">
         <?php PageHeaderBuilder::create(__('account', 'login'))->setBackButton('index.php')->build(); ?>
@@ -23,7 +22,7 @@ if(!isset($_POST['login_user']) || !isset($_POST['login_pass'])) {
 
             <form method="POST" action="login.php?a=login">
                 <input type="text" name="login_user" value="" placeholder="<?= __('account', 'username'); ?>" />
-                <input type="password" name="login_pass" value=""
+                <input type="password" name="login_password" value=""
                        placeholder="<?= __('account', 'password'); ?>" /><br />
 
                 <input type="submit" value="<?= __('account', 'login'); ?>"
@@ -37,68 +36,59 @@ if(!isset($_POST['login_user']) || !isset($_POST['login_pass'])) {
 
 } else {
 
-    // TODO: Temporarily show an error page
-    showErrorPage();
+    // Get the username/email and password
+    $loginUser = trim($_POST['login_user']);
+    $loginPassword = $_POST['login_password'];
 
-    $loginUser = $_POST['login_user'];
-    $loginPass = $_POST['login_pass'];
+    // Define a variable to get the user
+    $user = null;
 
-    // Make sure a team exists with this ID
-    if(!TeamManager::isTeamWithId($loginUser))
-        $error = true;
+    // Check whether a user exists with this username
+    if(UserManager::isUserWithUsername($loginUser))
+        $user = UserManager::getUserWithUsername($loginUser);
 
-    else
-        $team = new Team($loginUser);
+    elseif(AccountUtils::isValidMail($loginUser) && \app\mail\MailManager::isMailWithMail($loginUser)) {
+        // Get the mail of the user
+        $mail = MailManager::getMailWithMail($loginUser);
+
+        // Get the corresponding user if valid
+        if($mail !== null)
+            $user = $mail->getUser();
+    }
+
+    // TODO: Select users with mails still needing to be verified (only if once user is returned)
+
+    // Make sure a user is found
+    if(!($user instanceof User))
+        showErrorPage(__('login', 'usernameOrPasswordIncorrect'));
 
     // Validate the password
-    $passCorrect = $team->isPassword($loginPass);
+    if(!$user->isPassword($loginPassword))
+        showErrorPage(__('login', 'usernameOrPasswordIncorrect'));
 
-    if(!$passCorrect):
-        ?>
-        <div data-role="page" id="page-main">
-            <?php PageHeaderBuilder::create(__('error', 'oops'))->setBackButton('index.php')->build(); ?>
+    // Create a session for the user
+    if(!SessionManager::createSession($user))
+        showErrorPage();
 
-            <div data-role="main" class="ui-content">
-                <p><?= __('login', 'usernameOrPasswordIncorrect'); ?><br /><?= __('error', 'goBackTryAgain'); ?></p>
-                <br />
+    ?>
+    <div data-role="page" id="page-main">
+        <?php PageHeaderBuilder::create()->build(); ?>
 
-                <fieldset data-role="controlgroup" data-type="vertical">
-                    <a href="index.php" data-rel="back" class="ui-btn ui-icon-back ui-btn-icon-left"
-                       data-direction="reverse"><?= __('navigation', 'goBack'); ?></a>
-                </fieldset>
-            </div>
+        <div data-role="main" class="ui-content">
+            <p>
+                <?= __('general', 'welcome'); ?> <?=$user->getFullName(); ?>!<br /><?= __('login', 'loginSuccess'); ?>
+            </p>
+            <br />
 
-            <?php PageFooterBuilder::create()->build(); ?>
+            <fieldset data-role="controlgroup" data-type="vertical">
+                <a href="index.php" data-ajax="false"
+                   class="ui-btn ui-icon-carat-r ui-btn-icon-left"><?= __('navigation', 'continue'); ?></a>
+            </fieldset>
         </div>
-        <?php
-    else:
 
-        // Create a session for the current user
-        SessionManager::createSession($team);
-
-        ?>
-        <div data-role="page" id="page-main">
-            <?php PageHeaderBuilder::create()->build(); ?>
-
-            <div data-role="main" class="ui-content">
-                <p>
-                    <?= __('general', 'welcome'); ?>!<br /><?= __('login', 'loginSuccess'); ?>
-
-                    <!-- TODO: Add some additional description here! -->
-                </p>
-                <br />
-
-                <fieldset data-role="controlgroup" data-type="vertical">
-                    <a href="index.php" data-ajax="false"
-                       class="ui-btn ui-icon-carat-r ui-btn-icon-left"><?= __('navigation', 'continue'); ?></a>
-                </fieldset>
-            </div>
-
-            <?php PageFooterBuilder::create()->build(); ?>
-        </div>
-        <?php
-
-    endif;
+        <?php PageFooterBuilder::create()->build(); ?>
+    </div>
+    <?php
 }
 
 // Include the page bottom
