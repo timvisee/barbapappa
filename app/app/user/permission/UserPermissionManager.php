7bef8c11ef9d9d1cf0caf8bd52691b2e7583d0c5
Tim Visée <timvisee@gmail.com>
2015-09-23 22:41:16 +0200

    Created base user permission manager class

---
+++ app/app/user/permission/UserPermissionManager.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace app\user\permission;
+
+// Prevent direct requests to this file due to security reasons
+defined('APP_INIT') or die('Access denied!');
+
+class UserPermissionManager { }
