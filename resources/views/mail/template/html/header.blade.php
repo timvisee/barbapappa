5d3e0f38b5495490955b5174615ecad60d437d13
timvisee <timvisee@gmail.com>
2017-09-07 22:58:43 +0200

    Fix logo rendering in email, create Logo facade

--- resources/views/mail/template/html/header.blade.php
+++ resources/views/mail/template/html/header.blade.php
@@ -21,7 +21,7 @@
                                     <td class="six sub-columns"
                                         style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; text-align: left; min-width: 0px; width: 50%; color: #222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 0px 10px 10px 0px;"
                                         align="left" valign="top">
-                                        {{ logo()->element(false, [
+                                        {{ app('logo')->element(false, [
                                             'style' => 'outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; width: auto; max-width: 100%; float: left; clear: both; display: block; height: 36px;',
                                             'align' => 'left',
                                         ]) }}</td>
