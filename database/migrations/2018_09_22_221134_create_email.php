4d7e6f32e56973cb185c00effcebe2a743209188
timvisee <timvisee@gmail.com>
2018-09-23 00:49:07 +0200

    Remove property from email migration

--- database/migrations/2018_09_22_221134_create_email.php
+++ database/migrations/2018_09_22_221134_create_email.php
@@ -16,7 +16,6 @@ class CreateEmail extends Migration {
             $table->increments('id');
             $table->integer('user_id');
             $table->string('email');
-            $table->string('token');
             $table->datetime('verified_at')->nullable();
             $table->string('verified_ip')->nullable();
             $table->rememberToken();
