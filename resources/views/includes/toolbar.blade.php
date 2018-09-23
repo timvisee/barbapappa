c74ba30efe07675b167164fcaadc2d215f1546f5
timvisee <timvisee@gmail.com>
2018-09-23 15:00:57 +0200

    Define custom toolbar style

--- resources/views/includes/toolbar.blade.php
+++ resources/views/includes/toolbar.blade.php
@@ -1,15 +1,13 @@
-{{-- Optional header style: border-top: none; border-bottom: 1px solid #CCCCCC; --}}
-
-<div id="header" data-role="header">
+<div class="toolbar">
 
     <div class="left">
         <a id="sidebar-toggle" href="#sidebar-panel" class="glyphicons glyphicons-menu-hamburger"></a>
     </div>
 
     <h1>
-        <a href="{{ route('index') }}" data-ajax="false" title="Refresh app">
+        <a href="{{ route('index') }}" title="Refresh app">
             {{-- TODO: Use a properly sized image here --}}
-            {{ logo()->element(false, ['style' => 'height: 21px; display: block;']) }}
+            {{ logo()->element(false) }}
         </a>
     </h1>
 
