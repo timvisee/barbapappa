e782caca6413e136dd7610512f2edbfed88f80d7
timvisee <timvisee@gmail.com>
2018-09-23 17:16:01 +0200

    Style login form and inline alerts

--- resources/views/error/inline.blade.php
+++ resources/views/error/inline.blade.php
@@ -1,5 +1,6 @@
 @foreach($messages as $message)
-    <div class="alert alert-danger">
-        <span class="halflings halflings-exclamation-sign icon"></span> {{ $message }}
+    <div class="ui error message small visible">
+        <span class="halflings halflings-exclamation-sign icon"></span>
+        {{ $message }}
     </div>
 @endforeach
--- resources/views/myauth/login.blade.php
+++ resources/views/myauth/login.blade.php
@@ -2,27 +2,24 @@
 
 @section('content')
 
-    {!! Form::open(['action' => ['LoginController@doLogin'], 'method' => 'POST', 'data-ajax' => 'false']) !!}
+    {!! Form::open(['action' => ['LoginController@doLogin'], 'method' => 'POST', 'class' =>'ui form']) !!}
 
-    <div class="ui-field-contain">
+    <div class="field {{ ErrorRenderer::hasError('email') ? 'error' : '' }}">
         {{ Form::label('email', __('account.email') . ':') }}
         {{ Form::text('email', '', ['placeholder' => __('account.emailPlaceholder')]) }}
         {{ ErrorRenderer::inline('email') }}
     </div>
 
-    <div class="ui-field-contain">
+    <div class="field {{ ErrorRenderer::hasError('password') ? 'error' : '' }}">
         {{ Form::label('password', __('account.password') . ':') }}
         {{ Form::password('password') }}
         {{ ErrorRenderer::inline('password') }}
     </div>
 
-    <br />
-    {{ Form::submit(__('auth.login')) }}
-
-    <br />
-    <div data-role="controlgroup">
-        <a href="{{ route('password.request') }}" class="ui-btn ui-btn-corner-all">@lang('auth.forgotPassword')</a>
-        <a href="{{ route('register') }}" class="ui-btn ui-btn-corner-all">@lang('auth.register')</a>
+    <div data-role="ui buttons">
+        <button class="ui button primary" type="submit">@lang('auth.login')</button>
+        <a href="{{ route('password.request') }}" class="ui button basic">@lang('auth.forgotPassword')</a>
+        <a href="{{ route('register') }}" class="ui button basic">@lang('auth.register')</a>
     </div>
 
     {!! Form::close() !!}
