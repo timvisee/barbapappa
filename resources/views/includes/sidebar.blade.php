0fa174d29fa56fe857806371c09e34b11028bac4
timvisee <timvisee@gmail.com>
2018-09-30 22:23:38 +0200

    Patch some glyphicon rendering issues with theme, update styles

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,36 +1,36 @@
 <div class="ui sidebar inverted vertical menu">
     @if(barauth()->isAuth())
         <a href="{{ route('dashboard') }}" class="item">
-            <i class="icon-glyphicons glyphicons glyphicons-dashboard"></i>
+            <i class="glyphicons glyphicons-dashboard"></i>
             @lang('pages.dashboard')
         </a>
         <a href="{{ route('community.overview') }}" class="item">
-            <i class="icon-glyphicons glyphicons glyphicons-group"></i>
+            <i class="glyphicons glyphicons-group"></i>
             @lang('pages.communities')
         </a>
         <a href="{{ route('bar.overview') }}" class="item">
-            <i class="icon-glyphicons glyphicons glyphicons-beer"></i>
+            <i class="glyphicons glyphicons-beer"></i>
             @lang('pages.bars')
         </a>
         <a href="{{ route('account') }}" class="item">
-            <i class="icon-glyphicons glyphicons glyphicons-user"></i>
+            <i class="glyphicons glyphicons-user"></i>
             @lang('pages.account')
         </a>
         <a href="{{ route('logout') }}" class="item red">
-            <i class="icon-glyphicons glyphicons glyphicons-exit"></i>
+            <i class="glyphicons glyphicons-exit"></i>
             @lang('auth.logout')
         </a>
     @else
         <a href="{{ route('index') }}" class="item">
-            <i class="icon-glyphicons glyphicons glyphicons-dashboard"></i>
+            <i class="glyphicons glyphicons-dashboard"></i>
             @lang('pages.dashboard')
         </a>
         <a href="{{ route('login') }}" class="item">
-            <i class="icon-glyphicons glyphicons glyphicons-user"></i>
+            <i class="glyphicons glyphicons-user"></i>
             @lang('auth.login')
         </a>
         <a href="{{ route('register') }}" class="item">
-            <i class="icon-glyphicons glyphicons glyphicons-user-asterisk"></i>
+            <i class="glyphicons glyphicons-user-asterisk"></i>
             @lang('auth.register')
         </a>
     @endif
@@ -39,19 +39,19 @@
         <div class="header">@lang('misc.information')</div>
         <div class="menu">
             <a href="{{ route('about') }}" class="item">
-                <i class="icon-glyphicons glyphicons glyphicons-heart-empty"></i>
+                <i class="glyphicons glyphicons-heart-empty"></i>
                 @lang('pages.about')
             </a>
             <a href="{{ route('terms') }}" class="item">
-                <i class="icon-glyphicons glyphicons glyphicons-handshake"></i>
+                <i class="glyphicons glyphicons-handshake"></i>
                 @lang('pages.terms')
             </a>
             <a href="{{ route('privacy') }}" class="item">
-                <i class="icon-glyphicons glyphicons glyphicons-{{ (langManager()->getLocale() != 'pirate' ? 'fingerprint' : 'skull') }}"></i>
+                <i class="glyphicons glyphicons-{{ (langManager()->getLocale() != 'pirate' ? 'fingerprint' : 'skull') }}"></i>
                 @lang('pages.privacy')
             </a>
             <a href="{{ route('contact') }}" class="item">
-                <i class="icon-glyphicons glyphicons glyphicons-send"></i>
+                <i class="glyphicons glyphicons-send"></i>
                 @lang('pages.contact')
             </a>
             <a href="{{ route('language') }}" class="item">
