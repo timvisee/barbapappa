12bb28e2867c287984b0e242f11980f96cd733de
timvisee <timvisee@gmail.com>
2018-12-15 21:32:51 +0100

    Add product and some mutation migrations

---
+++ webapp/database/migrations/2018_12_15_192039_create_mutation_wallet_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMutationWalletTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('mutations_wallet', function(Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('mutation_id')->unsigned();
+            $table->integer('wallet_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('mutation_id')
+                ->references('id')
+                ->on('mutations')
+                ->onDelete('cascade');
+            $table->foreign('wallet_id')
+                ->references('id')
+                ->on('wallets')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('mutations_wallet');
+    }
+}
---
+++ webapp/database/migrations/2018_12_15_192040_create_mutation_payment_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMutationPaymentTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('mutations_payment', function(Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('mutation_id')->unsigned();
+            $table->integer('payment_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('mutation_id')
+                ->references('id')
+                ->on('mutations')
+                ->onDelete('cascade');
+            $table->foreign('payment_id')
+                ->references('id')
+                ->on('payments')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('mutations_payment');
+    }
+}
