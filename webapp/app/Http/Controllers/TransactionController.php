9e815d236295161ec058560e0e279c8d3906ceea
timvisee <tim@visee.me>
2019-04-20 21:32:26 +0200

    Implement basic permission checking for transaction viewing

--- webapp/app/Http/Controllers/TransactionController.php
+++ webapp/app/Http/Controllers/TransactionController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 use App\Helpers\ValidationDefaults;
+use App\Models\Mutation;
 use App\Models\Transaction;
 use App\Perms\Builder\Config as PermsConfig;
 use App\Perms\CommunityRoles;
@@ -27,8 +28,48 @@ class TransactionController extends Controller {
         $transaction = Transaction::findOrFail($transactionId);
         $mutations = $transaction->mutations();
 
+        // Check permission
+        if(!$this->hasPermission($transaction))
+            return response(view('noPermission'));
+
         return view('transaction.show')
             ->with('transaction', $transaction)
             ->with('mutations', $mutations->get());
+    }
+
+    /**
+     * Check whether the currently authenticated user has permission to view the
+     * given transaction.
+     *
+     * @param Transaction $transaction The transaction model.
+     *
+     * @return boolean True if the user can view this transaction, false if not.
+     */
+    function hasPermission(Transaction $transaction) {
+        // The user must be authenticated
+        $barauth = barauth();
+        if(!$barauth->isAuth())
+            return false;
+        $user = $barauth->getUser();
+
+        // If the current user initiated the transaction, it's all right
+        if($transaction->created_by == $user->id)
+            return true;
+
+        // Get all wallet mutations
+        $mutations = $transaction
+            ->mutations()
+            ->where('type', Mutation::TYPE_WALLET)
+            ->get();
+
+        // If the user owns any of the wallets, allow
+        foreach($mutations as $mutation)
+            if($mutation->mutationData->wallet->user_id == $user->id)
+                return true;
+
+        // TODO: allow management users in this economy
+
+        // Could not find link between transaction and authenticated user
+        return false;
     }
 }
