8d863923de7e7421953752df75e13c8169f0e6d6
timvisee <tim@visee.me>
2019-04-21 23:26:38 +0200

    Add custom Semantic UI style patches to improve tables on small screens

--- webapp/resources/views/layouts/app.blade.php
+++ webapp/resources/views/layouts/app.blade.php
@@ -18,8 +18,8 @@
     {{-- Styles --}}
     <link href="{{ asset('css/glyphicons-packed.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/flag-icon.css') }}" rel="stylesheet" />
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/semantic.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
 
     {{-- Scripts --}}
     <script type="text/javascript" src="{{ asset('js/jquery-packed.js') }}"></script>
