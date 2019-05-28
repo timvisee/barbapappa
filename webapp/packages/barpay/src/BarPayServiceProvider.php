50aed92e34e2725814a495899e7c509c08e7c3b6
timvisee <tim@visee.me>
2019-05-28 21:11:11 +0200

    Scaffold custom payment processing package

---
+++ webapp/packages/barpay/src/BarPayServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BarPay;
+
+use Illuminate\Support\ServiceProvider;
+
+class BarPayServiceProvider extends ServiceProvider {
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register() {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot() {
+        // $this->loadViewsFrom(__DIR__ . '/views', 'pay');
+        // $this->publishes([
+        //     __DIR__.'/views' => base_path('resources/views/timvisee/pay'),
+        // ]);
+    }
+}
