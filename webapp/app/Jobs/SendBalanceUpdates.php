9fa9d6a88060e8265a2e60310935af6484355cf7
timvisee <tim@visee.me>
2019-08-17 16:35:52 +0200

    Extract balance update check and sending to separate jobs

--- webapp/app/Jobs/SendBalanceUpdates.php
+++ webapp/app/Jobs/SendBalanceUpdates.php
@@ -3,20 +3,19 @@
 namespace App\Jobs;
 
 use App\Mail\Update\BalanceUpdateMail;
-use App\Models\Community;
-use App\Models\Economy;
 use App\Models\EmailHistory;
-use App\Models\Notifications\Notification;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 
 /**
- * Send an update to all users each month, with their current wallet balance.
+ * Check for each user whether a new balance update should be sent, and in that
+ * case, schedule a job to do so.
  */
 class SendBalanceUpdates implements ShouldQueue {
 
@@ -80,63 +79,18 @@ class SendBalanceUpdates implements ShouldQueue {
 
         // Send an update to each listed user
         $users->each(function($user) {
-            // Collect wallet balances for user, group by economy
-            $wallets = $user
-                ->wallets
-                ->groupBy('economy_id');
+            DB::transaction(function() use($user) {
+                // Schedule a job to send the balance update for the user
+                SendBalanceUpdate::dispatch($user->id);
 
-            // Get all economies for the user wallets
-            $economyIds = $wallets->keys();
-            $economies = Economy::whereIn('id', $economyIds)->get();
-
-            // Get all communities for the user wallet
-            $communityIds = $economies->pluck('community_id')->unique();
-            $communities = Community::whereIn('id', $communityIds)->get();
-
-            // Build the data object with all community/economy/wallet
-            // information used in the balance mail template
-            $data = $communities
-                ->map(function($community) use($economies, $wallets) {
-                    $economyData = $economies
-                        ->where('community_id', $community->id)
-                        ->map(function($economy) use($wallets) {
-                            // Build the wallet data
-                            $walletData = $wallets
-                                ->get($economy->id)
-                                ->map(function($wallet) {
-                                    return [
-                                        'name' => $wallet->name,
-                                        'balance' => $wallet->formatBalance(),
-                                        'balanceHtml' => $wallet->formatBalance(BALANCE_FORMAT_COLOR),
-                                    ];
-                                });
-
-                            // Build economy object with wallets data
-                            return [
-                                'name' => $economy->name,
-                                'wallets' => $walletData,
-                            ];
-                        });
-
-                    // Build community object with economy data
-                    return [
-                        'name' => $community->name,
-                        'economies' => $economyData,
-                    ];
-                })->toArray();
-
-            // Send the balance update mail
-            Mail::send(
-                new BalanceUpdateMail($user->buildEmailRecipients(), $data)
-            );
-
-            // Update email history time
-            EmailHistory::updateOrCreate([
-                'user_id' => $user->id,
-                'type' => EmailHistory::TYPE_BALANCE_UPDATE,
-            ], [
-                'last_at' => now(),
-            ]);
+                // Update email history time
+                EmailHistory::updateOrCreate([
+                    'user_id' => $user->id,
+                    'type' => EmailHistory::TYPE_BALANCE_UPDATE,
+                ], [
+                    'last_at' => now(),
+                ]);
+            });
         });
     }
 }
