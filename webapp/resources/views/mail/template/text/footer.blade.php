3b99ab33029efddf1401eb00a4ca5ee30dbd34ff
timvisee <tim@visee.me>
2019-08-17 16:38:23 +0200

    Remove big footer from email template, as it's still unused

--- webapp/resources/views/mail/template/text/footer.blade.php
+++ webapp/resources/views/mail/template/text/footer.blade.php
@@ -1,10 +1,6 @@
 ----------------------------------------------------------------------
 
-My balance: €123,00
-
 @lang('pages.dashboard.title'): {{ route('dashboard') }}
-@lang('pages.yourAccount'): {{ route('account') }}
-
 @lang('pages.terms.title'): {{ route('terms') }}
 @lang('pages.privacy.title'): {{ route('privacy') }}
 @lang('pages.emailPreferences'): {{ route('email.preferences') }}
