ed81e5e49a5da14306b14a6a954ac40c57050931
timvisee <tim@visee.me>
2019-08-22 13:26:29 +0200

    Add simple wizard text to advanced buy page

--- webapp/resources/js/components/Buy.vue
+++ webapp/resources/js/components/Buy.vue
@@ -2,6 +2,18 @@
 
 <template>
     <div>
+        <center>
+            <p v-if="selected.length == 0 && cart.length == 0">
+                Tap products to buy for any user.
+            </p>
+            <p v-else-if="cart.length == 0">
+                Tap users to add the selected products in cart for.
+            </p>
+            <p v-else>
+                Press the blue buy button to commit the purchase.
+            </p>
+        </center>
+
         <Product-Select :selected="selected"></Product-Select>
         <User-Chooser v-if="selected.length > 0" :selected="selected" :cart="cart"></User-Chooser>
         <Cart v-if="cart.length > 0" :cart="cart"></Cart>
