f1aff86ace72076b06aacd189d88670680a4adb0
timvisee <tim@visee.me>
2019-09-01 18:24:51 +0200

    Simplify payment details page, show state icon to make it more clear

--- webapp/resources/views/payment/show.blade.php
+++ webapp/resources/views/payment/show.blade.php
@@ -3,6 +3,8 @@
 @section('title', __('pages.payments.details'))
 
 @php
+    use \BarPay\Models\Payment;
+
     // Define menulinks
     $menulinks[] = [
         'name' => __('pages.payments.backToPayments'),
@@ -52,22 +54,105 @@
         </div>
     @endif
 
+    <div class="ui divider hidden"></div>
+
+    {{-- Payment state icon --}}
+    <div class="ui one small statistics">
+        @switch($payment->state)
+            @case(Payment::STATE_INIT)
+            @case(Payment::STATE_PENDING_COMMUNITY)
+            @case(Payment::STATE_PENDING_AUTO)
+                <div class="statistic yellow">
+                    <div class="value">
+                        <span class="halflings halflings-hourglass" title="{{ $payment->stateName() }}"></span>
+                    </div>
+                    <div class="label">@lang('misc.state')</div>
+                </div>
+                @break
+
+            @case(Payment::STATE_PENDING_USER)
+                <div class="statistic orange">
+                    <div class="value">
+                        <span class="halflings halflings-user" title="{{ $payment->stateName() }}"></span>
+                    </div>
+                    <div class="label">@lang('misc.state')</div>
+                </div>
+                @break
+
+            @case(Payment::STATE_PROCESSING)
+                <div class="statistic yellow">
+                    <div class="value">
+                        <span class="halflings halflings-refresh" title="{{ $payment->stateName() }}"></span>
+                    </div>
+                    <div class="label">@lang('misc.state')</div>
+                </div>
+                @break
+
+            @case(Payment::STATE_COMPLETED)
+                <div class="statistic green">
+                    <div class="value">
+                        <span class="halflings halflings-ok" title="{{ $payment->stateName() }}"></span>
+                    </div>
+                    <div class="label">@lang('misc.state')</div>
+                </div>
+                @break
+
+            @case(Payment::STATE_REVOKED)
+            @case(Payment::STATE_REJECTED)
+                <div class="statistic red">
+                    <div class="value">
+                        <span class="halflings halflings-remove" title="{{ $payment->stateName() }}"></span>
+                    </div>
+                    <div class="label">@lang('misc.state')</div>
+                </div>
+                @break
+
+            @case(Payment::STATE_FAILED)
+                <div class="statistic red">
+                    <div class="value">
+                        <span class="halflings halflings-alert" title="{{ $payment->stateName() }}"></span>
+                    </div>
+                    <div class="label">@lang('misc.state')</div>
+                </div>
+                @break
+
+            @default
+                <div class="statistic">
+                    <div class="value">
+                        {{ $payment->stateName() }}
+                    </div>
+                    <div class="label">@lang('misc.state')</div>
+                </div>
+        @endswitch
+    </div>
+    <br>
+    <div class="ui one small statistics">
+        <div class="statistic">
+            <div class="value">
+                {!! $payment->formatCost(BALANCE_FORMAT_COLOR) !!}
+            </div>
+            <div class="label">@lang('misc.amount')</div>
+        </div>
+    </div>
+
+    <div class="ui divider hidden"></div>
+
     <table class="ui compact celled definition table">
         <tbody>
+            <tr>
+                <td>@lang('misc.state')</td>
+                <td>{{ $payment->stateName() }}</td>
+            </tr>
+            <tr>
+                <td>@lang('pages.paymentService.serviceType')</td>
+                <td>{{ $payment->service->displayName() }}</td>
+            </tr>
             @if($payment->user_id == barauth()->getUser()->id)
                 <tr>
                     <td>@lang('misc.user')</td>
                     <td>{{ $payment->user->name }}</td>
                 </tr>
             @endif
-            <tr>
-                <td>@lang('misc.amount')</td>
-                <td>{!! $payment->formatCost(BALANCE_FORMAT_COLOR) !!}</td>
-            </tr>
-            <tr>
-                <td>@lang('misc.state')</td>
-                <td>{{ $payment->stateName() }}</td>
-            </tr>
             <tr>
                 <td>@lang('misc.initiatedAt')</td>
                 <td>@include('includes.humanTimeDiff', ['time' => $payment->created_at])</td>
@@ -78,30 +163,10 @@
                     <td>@include('includes.humanTimeDiff', ['time' => $payment->updated_at])</td>
                 </tr>
             @endif
-            <tr>
-                <td>@lang('pages.paymentService.serviceType')</td>
-                <td>{{ $payment->service->displayName() }}</td>
-            </tr>
         </tbody>
     </table>
 
-    {{-- Action buttons --}}
-    @if($payment->isInProgress())
-        <div class="ui buttons">
-            @if($payment->isInProgress())
-                <a href="{{ route('payment.pay', ['paymentId' => $payment->id]) }}"
-                        class="ui button positive">
-                    @lang('misc.progress')
-                </a>
-            @endif
-            @if($payment->canCancel())
-                <a href="{{ route('payment.cancel', ['paymentId' => $payment->id]) }}"
-                        class="ui button negative">
-                    @lang('general.cancel')
-                </a>
-            @endif
-        </div>
-    @endif
+    <div class="ui divider hidden"></div>
 
     {{-- Show link to transaction --}}
     @if(!empty($transaction))
@@ -125,6 +190,26 @@
                     @include('includes.humanTimeDiff', ['time' => $transaction->updated_at ?? $transaction->created_at])
                 </span>
             </a>
+        </div>
+    @endif
+
+    <div class="ui divider hidden"></div>
+
+    {{-- Action buttons --}}
+    @if($payment->isInProgress())
+        <div class="ui buttons">
+            @if($payment->isInProgress())
+                <a href="{{ route('payment.pay', ['paymentId' => $payment->id]) }}"
+                        class="ui button positive">
+                    @lang('misc.progress')
+                </a>
+            @endif
+            @if($payment->canCancel())
+                <a href="{{ route('payment.cancel', ['paymentId' => $payment->id]) }}"
+                        class="ui button negative">
+                    @lang('general.cancel')
+                </a>
+            @endif
         </div>
     @endif
 
