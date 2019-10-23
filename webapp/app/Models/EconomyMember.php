89294cc73febb32fd408b10ae60272b204847686
timvisee <tim@visee.me>
2019-10-23 20:45:05 +0200

    Create economy member model with migration

---
+++ webapp/app/Models/EconomyMember.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+/**
+ * Economy member model.
+ *
+ * @property int id
+ * @property int economy_id
+ * @property-read Economy economy
+ * @property int user_id
+ * @property-read user
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ */
+class EconomyMember extends Pivot {
+
+    protected $table = 'economy_member';
+
+    protected $with = ['user'];
+
+    /**
+     * Get dynamic properties.
+     *
+     * @param string $name Property name.
+     *
+     * @return mixed|string Result.
+     */
+    public function __get($name) {
+        switch($name) {
+            case 'name':
+                return $this->user->name;
+            default:
+                return parent::__get($name);
+        }
+    }
+
+    /**
+     * Check whether dynamic properties exist.
+     *
+     * @param string $name Property name.
+     *
+     * @return bool True if exists, false if not.
+     */
+    public function __isset($name) {
+        switch($name) {
+            case 'name':
+                return true;
+            default:
+                return parent::__isset($name);
+        }
+    }
+
+    /**
+     * Get the member economy.
+     *
+     * @return Economy The economy.
+     */
+    public function economy() {
+        return $this->belongsTo(Economy::class);
+    }
+
+    /**
+     * Get the member user.
+     *
+     * @return User The user.
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ webapp/database/migrations/2018_09_24_183121_create_economy_member_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEconomyMemberTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('economy_member', function(Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('economy_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('economy_id')
+                ->references('id')
+                ->on('economies')
+                ->onDelete('cascade');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->unique(['economy_id', 'user_id']);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('economy_member');
+    }
+}
