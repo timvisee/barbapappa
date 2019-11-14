1c27e6f228eab26e84b330cde8227b0e0da5e862
timvisee <tim@visee.me>
2019-11-14 18:26:02 +0100

    Add currency formatting function

--- webapp/app/Models/NewCurrency.php
+++ webapp/app/Models/NewCurrency.php
@@ -93,6 +93,48 @@ class NewCurrency extends Model {
     }
 
     /**
+     * Format the given value as this currency.
+     *
+     * @param float $value The value to format.
+     * @return string The formatted value.
+     */
+    public function format($value) {
+        // Get the measurement format
+        $format = $this->format;
+
+        // Value Regex
+        $valRegex = '/([0-9].*|)[0-9]/';
+
+        // Match decimal and thousand separators
+        preg_match_all('/[\s\',.!]/', $format, $separators);
+        if($thousand = Arr::get($separators, '0.0', null))
+            if($thousand == '!')
+                $thousand = '';
+        $decimal = Arr::get($separators, '0.1', null);
+
+        // Match format for decimals count
+        preg_match($valRegex, $format, $valFormat);
+        $valFormat = Arr::get($valFormat, 0, 0);
+
+        // Count decimals length
+        $decimals = $decimal ? strlen(substr(strrchr($valFormat, $decimal), 1)) : 0;
+
+        // Do we have a negative value?
+        if($negative = $value < 0 ? '-' : '')
+            $value = $value * -1;
+
+        // Format the value
+        $value = number_format($value, $decimals, $decimal, $thousand);
+
+        // Apply the formatted measurement
+        if($include_symbol)
+            $value = preg_replace($valRegex, $value, $format);
+
+        // Return value
+        return $negative . $value;
+    }
+
+    /**
      * Get a list of known currency codes following ISO 4217.
      * These are stored in 'resources/data/currency-codes.txt'.
      *
