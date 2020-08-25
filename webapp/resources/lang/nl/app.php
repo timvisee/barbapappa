5796f2cd438498398d41b7d9477f21206834b021
timvisee <tim@visee.me>
2020-08-25 16:22:11 +0200

    Add PWA install button, add logic to manage PWA prompts

---
+++ webapp/resources/lang/nl/app.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Application related translations.
+ */
+return [
+    'installApp' => 'App installeren',
+
+    // // Logo customization
+    // 'logo' => [
+    //     // Logo file names
+    //     'wrapFilename' => 'pirate_logo_wrap.svg',
+    //     'nowrapFilename' => 'pirate_logo_nowrap.svg',
+    //     'nowrapEmailFilename' => 'pirate_logo_nowrap_email.png',
+    // ]
+];
