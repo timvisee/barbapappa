4923a62d32c18e5b2132929f0ed7a71a58033c46
timvisee <tim@visee.me>
2020-11-30 19:37:44 +0100

    Remove unused imports

--- webapp/app/Mail/PersonalizedEmail.php
+++ webapp/app/Mail/PersonalizedEmail.php
@@ -3,12 +3,10 @@
 namespace App\Mail;
 
 use App\Utils\EmailRecipient;
-use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\App;
 
 abstract class PersonalizedEmail extends Mailable implements ShouldQueue {
 
--- webapp/app/Models/BalanceImportChange.php
+++ webapp/app/Models/BalanceImportChange.php
@@ -5,14 +5,6 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
-
-use App\Mail\Password\Reset;
-use App\Managers\PasswordResetManager;
-use App\Scopes\EnabledScope;
-use App\Utils\EmailRecipient;
 
 /**
  * Balance import change model.
