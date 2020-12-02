9c9ddabb55477827ffdfb1363d54f05d4785b4dc
timvisee <tim@visee.me>
2020-12-02 20:25:49 +0100

    Fix invalid function name, fix some Dutch translations

--- webapp/app/Models/BalanceImportChange.php
+++ webapp/app/Models/BalanceImportChange.php
@@ -413,7 +413,7 @@ class BalanceImportChange extends Model {
     public function formatCost($format = BALANCE_FORMAT_PLAIN) {
         if($this->cost == null)
             return null;
-        return $this->currency->formatAmount($this->cost, $format);
+        return $this->currency->format($this->cost, $format);
     }
 
     /**
