916f1e2b3dc34ce79add5977389b100a95566c33
timvisee <tim@visee.me>
2021-05-30 17:33:59 +0200

    Always show users having cart items in kiosk mode even if not in query

--- webapp/resources/js/kioskbuy/Users.vue
+++ webapp/resources/js/kioskbuy/Users.vue
@@ -44,6 +44,23 @@
             </div>
         </a>
 
+        <!-- Always show users having a cart on top if not part of query results -->
+        <a v-for="user in cart.map(c => c.user)"
+                v-if="!users.some(u => u.id == user.id) && !selectedUsers.some(u => u.id == user.id)"
+                v-on:click.prevent.stop="toggleSelectUser(user)"
+                v-bind:class="{ disabled: buying, active: isUserSelected(user) }"
+                href="#"
+                class="green item">
+            {{ user.name || __('misc.unknownUser') }}
+
+            <div class="right">
+                <span v-if="getQuantity(user) > 0" class="active green">{{ getQuantity(user) }}×</span>
+
+                <span v-if="isUserSelected(user)"
+                        class="item-icon glyphicons glyphicons-chevron-right"></span>
+            </div>
+        </a>
+
         <a v-for="user in users"
                 v-on:click.prevent.stop="toggleSelectUser(user)"
                 v-bind:class="{ disabled: buying, active: isUserSelected(user) }"
