effd08d2c357e8c0f1e3cab1b22083c3bac34e4a
timvisee <tim@visee.me>
2021-06-01 21:58:17 +0200

    Update asset preload list

--- webapp/resources/views/layouts/app.blade.php
+++ webapp/resources/views/layouts/app.blade.php
@@ -40,8 +40,9 @@
 
     {{-- Preloads/prefetches --}}
     <link rel="preload" href="{{ asset('sw.js') }}" as="worker">
-    <link rel="prefetch" href="{{ asset('fonts/glyphicons-regular.woff2') }}" as="font">
-    <link rel="prefetch" href="{{ asset('css/themes/default/assets/fonts/icons.woff2') }}" as="font">
+    <link rel="preload" href="{{ asset('fonts/glyphicons-regular.woff2') }}" as="font">
+    <link rel="preload" href="{{ asset('fonts/glyphicons-halflings-regular.woff2') }}" as="font">
+    <link rel="preload" href="{{ asset('css/themes/default/assets/fonts/icons.woff2') }}" as="font">
     <link rel="prefetch" href="{{ mix('js/widget/quickbuy.js') }}" as="script">
     <link rel="prefetch" href="{{ mix('js/widget/advancedbuy.js') }}" as="script">
 
