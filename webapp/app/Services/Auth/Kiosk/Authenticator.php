17d4e6ffd83b5133e345c06e725cd9c0629741d4
timvisee <tim@visee.me>
2021-06-17 22:27:25 +0200

    Remember user agent when starting kiosk session

--- webapp/app/Services/Auth/Kiosk/Authenticator.php
+++ webapp/app/Services/Auth/Kiosk/Authenticator.php
@@ -121,6 +121,7 @@ class Authenticator {
         // Generate an unique token and get the IP address
         $token = self::generateUniqueToken();
         $ip = Request::ip();
+        $userAgent = Request::userAgent();
         $expire = Carbon::now()->addSecond(self::SESSION_EXPIRE);
 
         // Create the new kiosk session object and save it
