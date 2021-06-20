e8fa2d8b4ed725a9e8f6fbd327d651b3472992f5
timvisee <tim@visee.me>
2021-06-20 19:27:29 +0200

    Improve breadcrumb hiding CSS selector, remove breadcrumb examples

--- webapp/routes/breadcrumbs.php
+++ webapp/routes/breadcrumbs.php
@@ -3,23 +3,6 @@
 use Diglactic\Breadcrumbs\Breadcrumbs;
 use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
 
-// // Home
-// Breadcrumbs::for('home', function (BreadcrumbTrail $trail) {
-//     $trail->push('Home', route('home'));
-// });
-
-// // Home > Blog
-// Breadcrumbs::for('blog', function (BreadcrumbTrail $trail) {
-//     $trail->parent('home');
-//     $trail->push('Blog', route('blog'));
-// });
-
-// // Home > Blog > [Category]
-// Breadcrumbs::for('category', function (BreadcrumbTrail $trail, $category) {
-//     $trail->parent('blog');
-//     $trail->push($category->title, route('category', $category));
-// });
-
 // Dashboard
 Breadcrumbs::for('dashboard', function(BreadcrumbTrail $trail) {
     $trail->push(__('pages.dashboard.title'), route('dashboard'));
