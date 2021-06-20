53277adc87912685f3271d10051f2e2a9f55a2b7
timvisee <tim@visee.me>
2021-06-20 19:43:08 +0200

    Link last breadcrumb, always show it black, move styles into patch file

--- webapp/resources/views/partials/breadcrumbs.blade.php
+++ webapp/resources/views/partials/breadcrumbs.blade.php
@@ -1,14 +1,20 @@
 @unless($breadcrumbs->isEmpty())
     <div class="ui breadcrumb">
         @foreach($breadcrumbs as $breadcrumb)
-
-            @if(!is_null($breadcrumb->url) && !$loop->last)
-                <a class="section" href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a>
-                <div class="divider"> / </div>
+            @if(!is_null($breadcrumb->url))
+                <a class="{{ $loop->last ? 'active' : '' }} section"
+                        href="{{ $breadcrumb->url }}">
+                    {{ $breadcrumb->title }}
+                </a>
             @else
-                <div class="active section">{{ $breadcrumb->title }}</div>
+                <div class="{{ $loop->last ? 'active' : '' }} section">
+                    {{ $breadcrumb->title }}
+                </div>
             @endif
 
+            @unless($loop->last)
+                <div class="divider"> / </div>
+            @endunless
         @endforeach
     </div>
 @endunless
