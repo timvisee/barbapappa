65524cf90982b71ee1e33081d65655777de38b18
timvisee <tim@visee.me>
2021-07-13 14:17:40 +0200

    Denote used timezone in absolute times

--- webapp/resources/views/includes/humanTimeDiff.blade.php
+++ webapp/resources/views/includes/humanTimeDiff.blade.php
@@ -1,9 +1,11 @@
 {{-- Render the difference between the given time and now in a humanly readable --}}
 {{-- format, use: `@include('includes.humanTimeDiff', ['time' => $time])` --}}
 
+{{-- TODO: use correct current timezone --}}
+
 @unless($absolute ?? false)
-    <span title="{{ $time }}"> {{ $time->diffForHumans(null, null, $short ?? false) }}</span>
+    <span title="{{ $time }} (UTC)"> {{ $time->diffForHumans(null, null, $short ?? false) }}</span>
 @else
-    <span title="{{ $time }}"> {{ $time->shortAbsoluteDiffForHumans(null, null, $short ?? false) }}</span>
+    <span title="{{ $time }} (UTC)"> {{ $time->shortAbsoluteDiffForHumans(null, null, $short ?? false) }}</span>
 @endif
 
