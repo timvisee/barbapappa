d2397cb424afa0544ffcf897d12a7306eb2ffebb
timvisee <tim@visee.me>
2021-07-29 14:27:34 +0200

    Bump version to 0.1.104

--- webapp/config/app.php
+++ webapp/config/app.php
@@ -17,8 +17,8 @@ return [
     /**
      * Application version name and code.
      */
-    'version_name' => '0.1.103',
-    'version_code' => 103,
+    'version_name' => '0.1.104',
+    'version_code' => 104,
 
     'author' => 'Tim Visée',
     'description' => 'Bar management application to manage transactions and inventory',
