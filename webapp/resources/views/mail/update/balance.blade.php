3520800b1a2214913d6f8d182bde434b14ae010f
timvisee <tim@visee.me>
2021-12-02 18:01:07 +0100

    Separate each wallet in blocks in balance update mail, improve text mail

--- webapp/resources/views/mail/update/balance.blade.php
+++ webapp/resources/views/mail/update/balance.blade.php
@@ -6,15 +6,17 @@
 
 <br>
 
+@component("mail::markdownOnly")---@endcomponent
+
 @foreach($data as $community)
 @foreach($community['economies'] as $economy)
-@component('mail::block')
-<b>{{ $community['name'] }}</b> ({{ $economy['name'] }}):
-
 @foreach($economy['wallets'] as $wallet)
-- [{{ $wallet['name'] }}]({{ $wallet['url'] }})  
-  @lang('misc.balance'): {!! $wallet['balanceHtml'] !!}  
-  @lang('misc.previously'): {!! $wallet['previousBalanceHtml'] !!} ({{ $wallet['previousPeriod'] }})  
+@component('mail::block')
+<strong>@component('mail::link', ['url' => $wallet['url']]){{ $wallet['name'] }}@endcomponent</strong><br><em>@lang('misc.in') {{ $community['name'] }} ({{ $economy['name'] }})</em>
+
+@lang('misc.balance'): {!! $wallet['balanceHtml'] !!}  
+@lang('misc.previously'): {!! $wallet['previousBalanceHtml'] !!} ({{ $wallet['previousPeriod'] }})  
+
 @component('mail::mini_button', ['url' => $wallet['topUpUrl'], 'color' => $wallet['isNegative'] ? 'blue' : 'grey'])
 @lang('misc.topUp')
 @endcomponent
@@ -25,8 +27,10 @@
 @lang('misc.stats')
 @endcomponent
 
-@endforeach
+@component("mail::markdownOnly")---@endcomponent
+
 @endcomponent
+@endforeach
 @endforeach
 @endforeach
 
