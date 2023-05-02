1d88769e7fdb8275b19b777ee4dea3aacf55bb04
timvisee <tim@visee.me>
2023-05-02 20:46:55 +0200

    Improve low balance message style, attach to toolbar on small screens

--- webapp/resources/views/bar/show.blade.php
+++ webapp/resources/views/bar/show.blade.php
@@ -20,7 +20,7 @@
 @section('content')
     {{-- Low balance message --}}
     @if(isset($userBalance) && $userBalance->amount < 0 && !empty($bar->low_balance_text))
-        <div class="ui error message">
+        <div class="ui error message attach-toolbar">
             <span class="halflings halflings-exclamation-sign icon"></span>
             {!! nl2br(e($bar->low_balance_text)) !!}
             <a href="{{ route('community.wallet.quickTopUp', [
