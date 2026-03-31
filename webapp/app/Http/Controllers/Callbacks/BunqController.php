b51b0c2864c49f03a6f1f3683b6af2a51cb3d609
timvisee <tim@visee.me>
2026-03-31 20:51:45 +0200

    On bunq me tab callback notification, immediately process it for payment

--- webapp/app/Http/Controllers/Callbacks/BunqController.php
+++ webapp/app/Http/Controllers/Callbacks/BunqController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Callbacks;
 
 use App\Jobs\ProcessBunqAccountEvents;
+use App\Jobs\ProcessBunqBunqMeTabEvent;
 use App\Models\BunqAccount;
+use bunq\Model\Generated\Endpoint\BunqMeTabApiObject;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use bunq\Model\Generated\Object\NotificationUrlObject;
@@ -43,15 +45,18 @@ class BunqController extends Controller {
             Self::processEventsForAccount($payment->getMonetaryAccountId(), 0);
         } else if(($bunqMeTab = $object->getBunqMeTab()) != null) {
             \Log::debug('bunq callback: processing bunq me tab payment');
-            Self::processEventsForAccount($bunqMeTab->getMonetaryAccountId(), 1);
+            Self::processBunqMeTab($bunqMeTab);
         } else if (($bunqMeTab = $object->getBunqMeTabResultInquiry()) != null || ($bunqMeTab = $object->getBunqMeTabResultResponse()) != null) {
             \Log::debug('bunq callback: processing bunq me tab payment inquiry');
             if(($payment = $bunqMeTab->getPayment()) != null)
                 if(($id = $payment->getMonetaryAccountId()) != null)
                     Self::processEventsForAccount($id, 1);
+
+            // Schedule bunq me tab processing as fallback in case events are incomplete
+            Self::processBunqMeTab($bunqMeTab, now()->addSeconds(15));
         } else if($object->getPaymentBatch() != null) {
-            \Log::debug('bunq callback: ignoring payment batch');
             // ignore payment batch
+            \Log::debug('bunq callback: ignoring payment batch');
         } else {
             throw new \Exception('Unhandled notification type');
         }
@@ -82,5 +87,34 @@ class BunqController extends Controller {
 
         // Dispatch a job for processing all new events on this monetary account
         ProcessBunqAccountEvents::dispatch($account, $retryCountIfNone);
+    }
+
+    /**
+     * Process a specific bunq me tab. Check of payment and process it if found.
+     *
+     * If no linked bunq account is found for the given monetary account ID it
+     * is ignored. Notifications are commonly received for monetary accounts not
+     * under control by Barbapappa.
+     *
+     * @param BunqMeTabApiObject $bunqMeTab The bunq me tab to process.
+     * @param Carbon|null $delay Optional delay for processing the bunq me tab,
+     *      used as fallback in case of delayed events on bunqs end.
+     */
+    // TODO: differentiate by production/sandbox account IDs
+    private static function processBunqMeTab(BunqMeTabApiObject $bunqMeTab, $delay = null) {
+        $monetaryAccountId = $bunqMeTab->getMonetaryAccountId();
+        if($monetaryAccountId == null)
+            return;
+
+        // Find the account, skip if it is not linked
+        // TODO: handle deleted accounts here as well?
+        $account = BunqAccount::where('monetary_account_id', $monetaryAccountId)->first();
+        if($account == null)
+            return;
+
+        // Dispatch a job for processing events on the bunq me tab
+        $job = ProcessBunqBunqMeTabEvent::dispatch($account, $bunqMeTab);
+        if($delay != null)
+            $job->delay($delay);
     }
 }
